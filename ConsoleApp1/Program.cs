8360111e569c58cfc0b95602a7ef4f3297d1671e
Stas <st520vip20@gmail.com>
2020-08-18 22:09:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello, " + name);
+            Thread.Sleep(5000);
+            Console.WriteLine("Goodbye, " + name);
+            Console.ReadKey();
+        }
+    }
+}
